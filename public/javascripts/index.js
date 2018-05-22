e3e1ec0edd199803b252dcddb0cb53a9ef0524e3
Steven Liu <fengcanliu@gmail.com>
2018-05-22 22:17:15 +1000

    adding cost V20

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
             b = a;
             a = x;
         }
+        console.log("+++++++++++a is "+a+"b is "+b);
         return this.storage[a][b];
     },
     put: function (a, b, value){
